a2ad2f298dfeaf88b5f681385187b50ec592508f
Reik Loeber <reik.loeber@gmail.com>
2026-01-11 17:13:26 +0100

    testfix url adapted

--- src/components/Nav/data.ts
+++ src/components/Nav/data.ts
@@ -3,7 +3,7 @@ const navData = {
     { label: 'About Me', to: '#about' },
     { label: 'My Skills', to: '#skills' },
     { label: 'My Projects', to: '#projects' },
-    { label: 'Docs', to: '/docs/guides/intro' },
+    { label: 'Docs', to: '/docs' },
     { label: 'Blog', to: '/blog' },
     { label: 'Contact Me', to: '#contact' }
   ]
